343ae439b9dfbd2b3e48e7b9e56e3a307a923fcf
besthor <igbebestor7@gmail.com>
2023-01-27 17:49:06 -0800

    what a zombie processs

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - creates an infinite loop to make the program hang
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	pid_t zombie;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie = fork();
+		if (!zombie)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
+
+	infinite_while();
+	return (0);
+}
