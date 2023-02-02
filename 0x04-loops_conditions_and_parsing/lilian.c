2d240559ebb85f27642cd8d0b39cb8e4a706c971
besthor <igbebestor7@gmail.com>
2023-02-02 06:29:43 -0800

    testing

---
+++ 0x04-loops_conditions_and_parsing/lilian.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print lilian
+ */
+
+int main(void)
+{
+	printf("Hello lilian, how are you doing today?\n");
+	printf("How is dominion.....how is Umuz!!\n");
+	printf("i hope you guys are fine \n");
+	printf("have a good day\n");
+	return(0);
+}
