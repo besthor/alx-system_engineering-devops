c7912ba94eac91e384db3e64a2db42be6127dc2a
besthor <igbebestor7@gmail.com>
2023-03-25 00:42:35 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 ##!/usr/bin/pup
 # Using Puppt to install flask from pip3
-
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem'
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
