d5e23fd95caf0bc218b5f329dbe91a337faa6847
Besthor <111004790+besthor@users.noreply.github.com>
2023-05-24 13:32:19 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,16 @@
-# Useing Puppet to automate a custom HTTP header response
-exec { 'update':
+# Using Puppet to automate a custom HTTP header response
+
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure => 'installed',
-  require => Exec['update'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file_line { 'http_header':
+-> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
-  line  => 'add_header X-Served-By $hostname;',
-  match => '^http {',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  hasstatus => true,
-  require   => [Package['nginx'], File_line['http_header']],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
