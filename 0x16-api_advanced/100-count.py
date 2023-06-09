2d4b174e81a604d96a67f97bfa8b3c0ea54dc7bb
Besthor <111004790+besthor@users.noreply.github.com>
2023-06-09 08:56:37 +0100

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,90 @@
 #!/usr/bin/python3
-'''Get ALL hot posts'''
-import pprint
-import re
+"""script for parsing web data from an api
+    DISCLAIMER: THIS PROBABLY SHOULDN'T BE DONE RECURSIVELY
+    but we had to for school :P
+"""
+import json
 import requests
+import sys
 
-BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
-
-def count_words(subreddit, word_list, hot_list=[], after=None):
-    '''Get ALL hot posts'''
-    headers = {'User-agent': 'Unix:0-subs:v1'}
-    params = {'limit': 100}
-    if isinstance(after, str):
-        if after != "STOP":
-            params['after'] = after
-        else:
-            return print_results(word_list, hot_list)
-
-    response = requests.get(BASE_URL.format(subreddit),
-                            headers=headers, params=params)
-    if response.status_code != 200:
+def get_hot_posts(subreddit, hot_list=[]):
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/{}/top.json'.format(
+        subreddit
+    )
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    if len(hot_list) == 0:
+        # grab initial list
+        url = base_url
+    else:
+        # grab next pagination after last obj in hot_list
+        url = base_url + '?after={}_{}'.format(
+            hot_list[-1].get('kind'),
+            hot_list[-1].get('data').get('id')
+        )
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+    try:
+        data = resp.get('data')
+        children = data.get('children')
+    except:
         return None
-    data = response.json().get('data', {})
-    after = data.get('after', 'STOP')
-    if not after:
-        after = "STOP"
-    hot_list = hot_list + [post.get('data', {}).get('title')
-                           for post in data.get('children', [])]
-    return count_words(subreddit, word_list, hot_list, after)
+    if children is None or data is None or len(children) < 1:
+        return hot_list
+    hot_list.extend(children)
+    return get_hot_posts(subreddit, hot_list)
 
 
-def print_results(word_list, hot_list):
-    '''Prints request results'''
-    count = {}
-    for word in word_list:
-        count[word] = 0
-    for title in hot_list:
-        for word in word_list:
-            count[word] = count[word] +\
-             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
+def count_words(subreddit, wordlist):
+    """count words in titles of hot posts for subreddit
+    """
+    posts = get_hot_posts(subreddit)
+    if posts is None:
+        print(end="")
+        return
+    words = gather_word_info(posts, wordlist)
+    sorted_list = [(key, val) for key, val in words.items()]
+    sorted_list = sorted(sorted_list, key=lambda tup: tup[1], reverse=True)
+    [print("{}: {}".format(key, val)) for (key, val) in sorted_list if val > 0]
 
-    count = {k: v for k, v in count.items() if v > 0}
-    words = sorted(list(count.keys()))
-    for word in sorted(words,
-                       reverse=True, key=lambda k: count[k]):
-        print("{}: {}".format(word, count[word]))
+
+def gather_word_info(hot_posts, wordlist,
+                     posts_len=None,
+                     counter=0,
+                     words_info=None):
+    """does the recursion to grab word info from wordlist and posts
+    """
+    if hot_posts is None:
+        return
+    # generate defaults
+    if posts_len is None:
+        posts_len = len(hot_posts)
+    if words_info is None:
+        words_info = {key: 0 for key in wordlist}
+    # base case
+    if counter == posts_len - 1:
+        return words_info
+
+    # parse this title and move to next
+    data = hot_posts[counter].get('data')
+    if data is None:
+        return words_info
+    title = data.get('title')
+    if title is None:
+        return words_info
+    # im sorry im not doing recursion for text parsing that's rediculous
+    for word in title.split(' '):
+        word = word.lower()
+        if word in wordlist:
+            words_info[word] += 1
+    counter += 1
+    return gather_word_info(
+        hot_posts, wordlist, posts_len,
+        counter, words_info
+    )
